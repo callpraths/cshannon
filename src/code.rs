0a305ba4b9ec7f9ab4faef6eb2653690e50a9558
Prathmesh Prabhu <callpraths@gmail.com>
2020-03-31 07:55:03 -0700

    Return a Vec<u8> from Alphabet::pack()
    Returning an iterator here is overkill -- the size of the pack()ed
    Alphabet is far smaller than the compressed Text.
    
    Also, using Iterators in the API is misleading: the implementation
    almost definitely iterates over the Alphabet multiple times to
    pack()/unpack().

--- src/code.rs
+++ src/code.rs
@@ -14,18 +14,55 @@ impl Alphabet {
     /// Serialize to a stream of bytes.
     ///
     /// Can be deserialized back to an Alphabet with pack().
-    pub fn pack(self) -> impl IntoIterator<Item = u8> {
-        PackedAlphabet::new(self.0)
+    pub fn pack(self) -> Vec<u8> {
+        let letter_count = self.0.len();
+        let letter_size_width = log_2(Alphabet::max_letter_size(&self.0) as u64);
+
+        let mut data: Vec<u8> = Vec::new();
+        data.append(&mut (letter_count as u64).to_be_bytes().to_vec());
+        data.append(&mut (letter_size_width).to_be_bytes().to_vec());
+        data.append(&mut Alphabet::pack_sizes(
+            letter_size_width as usize,
+            &self.0,
+        ));
+        data.append(&mut Alphabet::pack_letters(self.0));
+
+        data
     }
 
-    /// Parse a stream of bytes coded with this Alphabet into a Text.
-    ///
-    /// See Text::pack() for the reverse operation.
-    pub fn parse<T>(&self, _data: T) -> Text
-    where
-        T: IntoIterator<Item = u8>,
-    {
-        Text(Vec::new())
+    fn max_letter_size(letters: &Vec<Letter>) -> usize {
+        let mut m = 0;
+        for i in letters.iter() {
+            if i.len() > m {
+                m = i.len();
+            }
+        }
+        m
+    }
+
+    fn pack_sizes(width: usize, letters: &Vec<Letter>) -> Vec<u8> {
+        let mut sizes = BitVec::with_capacity(width * letters.len());
+        for l in letters.iter() {
+            let mut size = l.clone();
+            size.shrink_to_fit();
+            for _ in 0..(width - size.len()) {
+                sizes.push(false);
+            }
+            sizes.append(&mut size);
+        }
+        sizes.to_bytes()
+    }
+
+    fn pack_letters(letters: Vec<Letter>) -> Vec<u8> {
+        let mut packed = BitVec::with_capacity(Alphabet::total_size(&letters));
+        for mut l in letters.into_iter() {
+            packed.append(&mut l)
+        }
+        packed.to_bytes()
+    }
+
+    fn total_size(letters: &Vec<Letter>) -> usize {
+        letters.iter().fold(0, |s, i| s + i.len())
     }
 }
 
@@ -104,69 +141,15 @@ impl Alphabet {
     }
 }
 
-// TODO: Move all static methods to Alphabet and directly return Vec<u8> making
-// this struct redundant.
-struct PackedAlphabet(Vec<u8>);
-
-impl PackedAlphabet {
-    pub fn new(letters: Vec<Letter>) -> Self {
-        let letter_count = letters.len();
-        let letter_size_width = log_2(PackedAlphabet::max_letter_size(&letters) as u64);
-
-        let mut data: Vec<u8> = Vec::new();
-        data.append(&mut (letter_count as u64).to_be_bytes().to_vec());
-        data.append(&mut (letter_size_width).to_be_bytes().to_vec());
-        data.append(&mut PackedAlphabet::pack_sizes(
-            letter_size_width as usize,
-            &letters,
-        ));
-        data.append(&mut PackedAlphabet::pack_letters(letters));
-
-        Self(data)
-    }
-
-    fn max_letter_size(letters: &Vec<Letter>) -> usize {
-        let mut m = 0;
-        for i in letters.iter() {
-            if i.len() > m {
-                m = i.len();
-            }
-        }
-        m
-    }
-
-    fn pack_sizes(width: usize, letters: &Vec<Letter>) -> Vec<u8> {
-        let mut sizes = BitVec::with_capacity(width * letters.len());
-        for l in letters.iter() {
-            let mut size = l.clone();
-            size.shrink_to_fit();
-            for _ in 0..(width - size.len()) {
-                sizes.push(false);
-            }
-            sizes.append(&mut size);
-        }
-        sizes.to_bytes()
-    }
-
-    fn pack_letters(letters: Vec<Letter>) -> Vec<u8> {
-        let mut packed = BitVec::with_capacity(PackedAlphabet::total_size(&letters));
-        for mut l in letters.into_iter() {
-            packed.append(&mut l)
-        }
-        packed.to_bytes()
-    }
-
-    fn total_size(letters: &Vec<Letter>) -> usize {
-        letters.iter().fold(0, |s, i| s + i.len())
-    }
-}
-
-impl IntoIterator for PackedAlphabet {
-    type Item = u8;
-    type IntoIter = std::vec::IntoIter<u8>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
+impl Alphabet {
+    /// Parse a stream of bytes coded with this Alphabet into a Text.
+    ///
+    /// See Text::pack() for the reverse operation.
+    pub fn parse<T>(&self, _data: T) -> Text
+    where
+        T: IntoIterator<Item = u8>,
+    {
+        Text(Vec::new())
     }
 }
 
