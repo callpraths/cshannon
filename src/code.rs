2151dfdf719ac9bd1e15e7027e92738bcc35a8ab
Prathmesh Prabhu <callpraths@gmail.com>
2020-04-04 15:51:03 -0700

    Implement code::pack()

--- src/code.rs
+++ src/code.rs
@@ -1,4 +1,5 @@
 use std::convert::TryInto;
+use std::io::Write;
 use std::u64;
 
 /// core::result::Result alias with uniform error type.
@@ -132,12 +133,67 @@ where
 }
 
 /// Write a packed stream of letters.
-pub fn pack<I, W>(_i: I, _w: W) -> core::result::Result<usize, String>
+///
+/// Returns the number of bytes written.
+pub fn pack<I, W>(letters: I, w: W) -> core::result::Result<usize, String>
 where
     I: Iterator<Item = Letter>,
     W: std::io::Write,
 {
-    Err("not implemented".to_owned())
+    let mut bytes_written: usize = 0;
+    let mut bw = std::io::BufWriter::new(w);
+    let mut byte_buffer_len: u64 = 0; // In practice <= 7
+    let mut byte_buffer: u8 = 0;
+    for l in letters {
+        let mut has_more_bytes = true;
+        let mut remaining_bit_count = l.bit_count;
+        for b in l.data.into_iter() {
+            assert!(has_more_bytes);
+            byte_buffer |= b >> byte_buffer_len;
+            if byte_buffer_len + remaining_bit_count >= 8 {
+                bytes_written += bw.write(&[byte_buffer]).map_err(|e| e.to_string())?;
+
+                byte_buffer = safe_overflow_leftshift(b, 8 - byte_buffer_len);
+                remaining_bit_count -= 8;
+            } else {
+                byte_buffer_len += remaining_bit_count;
+                has_more_bytes = false;
+            }
+        }
+    }
+    if byte_buffer_len > 0 {
+        bytes_written += bw.write(&[byte_buffer]).map_err(|e| e.to_string())?;
+    }
+    bw.flush().map_err(|e| e.to_string())?;
+    Ok(bytes_written)
+}
+
+const RIGHT_MASK_7: u8 = 0b1;
+const RIGHT_MASK_6: u8 = 0b11;
+const RIGHT_MASK_5: u8 = 0b111;
+const RIGHT_MASK_4: u8 = 0b1111;
+const RIGHT_MASK_3: u8 = 0b11111;
+const RIGHT_MASK_2: u8 = 0b111111;
+const RIGHT_MASK_1: u8 = 0b1111111;
+
+// TODO: Generate with macros.
+fn safe_overflow_leftshift(b: u8, s: u64) -> u8 {
+    assert!(s <= 8);
+    if s == 8 {
+        return 0;
+    }
+    let masked = match s {
+        0 => b,
+        1 => b & RIGHT_MASK_1,
+        2 => b & RIGHT_MASK_2,
+        3 => b & RIGHT_MASK_3,
+        4 => b & RIGHT_MASK_4,
+        5 => b & RIGHT_MASK_5,
+        6 => b & RIGHT_MASK_6,
+        7 => b & RIGHT_MASK_7,
+        _ => panic!("No mask for {}", s),
+    };
+    masked << s
 }
 
 #[cfg(test)]
@@ -244,5 +300,100 @@ mod alphabet_tests {
         let packed = a.pack();
         let got = Alphabet::unpack(packed).unwrap();
         assert_eq!(got.0, v);
+    }
+}
+
+#[cfg(test)]
+mod pack_tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        let letters: Vec<Letter> = vec![];
+        let mut got = Vec::new();
+        assert_eq!(pack(letters.into_iter(), &mut got), Ok(0));
+        assert_eq!(got, Vec::new());
+    }
+
+    #[test]
+    fn zero_letter() {
+        let letters: Vec<Letter> = vec![Letter::from_bytes(&[])];
+        let mut got = Vec::new();
+        assert_eq!(pack(letters.into_iter(), &mut got), Ok(0));
+        assert_eq!(got, Vec::new());
+    }
+    #[test]
+    fn single_byte() {
+        let letters = vec![Letter::from_bytes(&[0x11])];
+        let mut got = Vec::new();
+        assert_eq!(pack(letters.into_iter(), &mut got), Ok(1));
+        assert_eq!(got, [0x11].to_vec());
+    }
+
+    #[test]
+    fn single_aligned_letter() {
+        let letters = vec![Letter::from_bytes(&[
+            0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa,
+        ])];
+        let mut got = Vec::new();
+        let want: Vec<u8> = vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa];
+        assert_eq!(pack(letters.into_iter(), &mut got), Ok(want.len()));
+        assert_eq!(got, want);
+    }
+
+    #[test]
+    fn single_unaligned_short_letter() {
+        let letters = vec![Letter {
+            data: vec![0b1101_1000],
+            bit_count: 5,
+        }];
+        let mut got = Vec::new();
+        let want: Vec<u8> = vec![0b1101_1000];
+        assert_eq!(pack(letters.into_iter(), &mut got), Ok(want.len()));
+        assert_eq!(got, want);
+    }
+
+    #[test]
+    fn single_unaligned_long_letter() {
+        let letters = vec![Letter {
+            data: vec![0b11011000, 0b11100000],
+            bit_count: 13,
+        }];
+        let mut got = Vec::new();
+        let want: Vec<u8> = vec![0b1101_1000, 0b1110_0000];
+        assert_eq!(pack(letters.into_iter(), &mut got), Ok(want.len()));
+        assert_eq!(got, want);
+    }
+
+    #[test]
+    fn multiple_aligned_letters() {
+        let letters = vec![
+            Letter::from_bytes(&[0x11, 0x22]),
+            Letter::from_bytes(&[0x33, 0x44, 0x55]),
+            Letter::from_bytes(&[0x66, 0x11]),
+        ];
+        let mut got = Vec::new();
+        let want: Vec<u8> = vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x11];
+        assert_eq!(pack(letters.into_iter(), &mut got), Ok(want.len()));
+        assert_eq!(got, want);
+    }
+
+    #[test]
+    fn multiple_unaligned_letters() {
+        let letters = vec![
+            Letter {
+                data: vec![0b1101_1000, 0b1000_0000],
+                bit_count: 9,
+            },
+            Letter {
+                data: vec![0b1101_0000],
+                bit_count: 4,
+            },
+            Letter::from_bytes(&[0b1101_1101]),
+        ];
+        let mut got = Vec::new();
+        let want: Vec<u8> = vec![0b1101_1000, 0b1110_1110, 0b1110_1000];
+        assert_eq!(pack(letters.into_iter(), &mut got), Ok(want.len()));
+        assert_eq!(got, want);
     }
 }
