78a60e10e2607865ae33d0be6572e05db53263c4
Prathmesh Prabhu <callpraths@gmail.com>
2020-04-05 23:41:22 -0700

    Implement Alphabet tree conversion

--- src/code.rs
+++ src/code.rs
@@ -1,4 +1,5 @@
 use std::convert::TryInto;
+use std::fmt;
 use std::io::Write;
 use std::u64;
 
@@ -11,8 +12,33 @@ type Result<R> = core::result::Result<R, String>;
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct Letter {
     data: Vec<u8>,
+    // TODO: Store as usize
     bit_count: u64,
 }
+
+impl fmt::Display for Letter {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for i in 0..(self.bit_count as usize) {
+            if self.at(i).unwrap() {
+                write!(f, "1")?;
+            } else {
+                write!(f, "0")?;
+            }
+        }
+        Ok(())
+    }
+}
+
+static BIT_HOLE_MASKS: [u8; 8] = [
+    0b1000_0000,
+    0b0100_0000,
+    0b0010_0000,
+    0b0001_0000,
+    0b0000_1000,
+    0b0000_0100,
+    0b0000_0010,
+    0b0000_0001,
+];
 
 impl Letter {
     pub fn from_bytes(bytes: &[u8]) -> Self {
@@ -20,6 +46,21 @@ impl Letter {
             bit_count: 8 * bytes.len() as u64,
             data: bytes.to_vec(),
         }
+    }
+
+    /// Return whether the bit at given index is set.
+    ///
+    /// Returns an error if the index is out of bounds.
+    pub fn at(&self, i: usize) -> Result<bool> {
+        if i as u64 >= self.bit_count {
+            return Err(format!(
+                "index {} out of bounds of letter sized {}",
+                i, self.bit_count,
+            ));
+        }
+        let b = i / 8;
+        let o = i % 8;
+        Ok(self.data[b] & BIT_HOLE_MASKS[o] > 0)
     }
 
     fn pack(mut self) -> Vec<u8> {
@@ -111,6 +152,117 @@ impl Alphabet {
     }
 }
 
+#[derive(Debug, PartialEq)]
+pub enum Node<'a> {
+    Internal {
+        zero: Option<Box<Node<'a>>>,
+        one: Option<Box<Node<'a>>>,
+    },
+    Leaf {
+        letter: &'a Letter,
+    },
+}
+
+impl Node<'_> {
+    pub fn new0<'a>(zero: Node<'a>) -> Node<'a> {
+        Node::Internal {
+            zero: Some(Box::new(zero)),
+            one: None,
+        }
+    }
+
+    pub fn new1<'a>(one: Node<'a>) -> Node<'a> {
+        Node::Internal {
+            zero: None,
+            one: Some(Box::new(one)),
+        }
+    }
+
+    pub fn newl<'a>(l: &'a Letter) -> Node<'a> {
+        Node::Leaf { letter: l }
+    }
+
+    pub fn new<'a>(zero: Node<'a>, one: Node<'a>) -> Node<'a> {
+        Node::Internal {
+            zero: Some(Box::new(zero)),
+            one: Some(Box::new(one)),
+        }
+    }
+}
+
+impl Alphabet {
+    pub fn tree<'a>(&'a self) -> Result<Node<'a>> {
+        if self.0.len() == 0 {
+            return Err("no letters".to_owned());
+        }
+        let mut root = Node::Internal {
+            zero: None,
+            one: None,
+        };
+        for l in self.0.iter() {
+            let (tip, offset) = Alphabet::follow_branch(&mut root, l, 0)?;
+            let tail = Alphabet::node(l, offset + 1);
+            match tip {
+                Node::Internal { zero, one } => {
+                    if l.at(offset)? {
+                        *one = Some(Box::new(tail));
+                    } else {
+                        *zero = Some(Box::new(tail));
+                    }
+                }
+                Node::Leaf { .. } => panic!("tip can not be a Leaf"),
+            }
+        }
+        Ok(root)
+    }
+
+    fn follow_branch<'a, 'b>(
+        tree: &'b mut Node<'a>,
+        l: &'a Letter,
+        offset: usize,
+    ) -> Result<(&'b mut Node<'a>, usize)> {
+        match l.at(offset) {
+            Ok(false) => {
+                match tree {
+                    Node::Internal {
+                        zero: Some(zero), ..
+                    } => {
+                        return Alphabet::follow_branch(zero, l, offset + 1);
+                    }
+                    // This error message actually needs a slice l[0:offset]
+                    Node::Leaf { .. } => return Err(format!("Duplicate prefix {}", l)),
+                    _ => return Ok((tree, offset)),
+                }
+            }
+            Ok(true) => {
+                match tree {
+                    Node::Internal { one: Some(one), .. } => {
+                        return Alphabet::follow_branch(one, l, offset + 1);
+                    }
+                    // This error message actually needs a slice l[0:offset]
+                    Node::Leaf { .. } => return Err(format!("Duplicate prefix {}", l)),
+                    _ => return Ok((tree, offset)),
+                }
+            }
+            Err(_) => Err(format!("Duplicate prefix {}", l)),
+        }
+    }
+
+    fn node<'a>(l: &'a Letter, offset: usize) -> Node<'a> {
+        match l.at(offset) {
+            Ok(false) => Node::Internal {
+                zero: Some(Box::new(Alphabet::node(l, offset + 1))),
+                one: None,
+            },
+            Ok(true) => Node::Internal {
+                zero: None,
+                one: Some(Box::new(Alphabet::node(l, offset + 1))),
+            },
+            Err(_) => Node::Leaf { letter: l },
+        }
+    }
+}
+
 /// An alphabet may be generated from an iterator over Letter.
 ///
 /// This operation clone()s the Letters.
@@ -197,7 +349,7 @@ fn safe_overflow_leftshift(b: u8, s: u64) -> u8 {
 }
 
 #[cfg(test)]
-mod alphabet_tests {
+mod alphabet_pack_tests {
     use super::*;
 
     #[test]
@@ -304,7 +456,7 @@ mod alphabet_tests {
 }
 
 #[cfg(test)]
-mod pack_tests {
+mod text_pack_tests {
     use super::*;
 
     #[test]
@@ -395,5 +547,109 @@ mod pack_tests {
         let want: Vec<u8> = vec![0b1101_1000, 0b1110_1110, 0b1110_1000];
         assert_eq!(pack(letters.into_iter(), &mut got), Ok(want.len()));
         assert_eq!(got, want);
+    }
+}
+
+#[cfg(test)]
+mod letter_tests {
+    use super::*;
+
+    #[test]
+    fn at() {
+        let l = Letter {
+            data: vec![0b0010_0010],
+            bit_count: 7,
+        };
+        assert_eq!(l.at(0).unwrap(), false);
+        assert_eq!(l.at(1).unwrap(), false);
+        assert_eq!(l.at(2).unwrap(), true);
+        assert_eq!(l.at(3).unwrap(), false);
+        assert_eq!(l.at(4).unwrap(), false);
+        assert_eq!(l.at(5).unwrap(), false);
+        assert_eq!(l.at(6).unwrap(), true);
+        assert!(l.at(7).is_err());
+    }
+}
+
+#[cfg(test)]
+mod alphabet_tree_tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        let a = Alphabet::new(vec![]);
+        assert!(a.tree().is_err())
+    }
+
+    #[test]
+    fn leaf0() {
+        let l = Letter {
+            data: vec![0b0],
+            bit_count: 1,
+        };
+        let a = Alphabet::new(vec![l.clone()]);
+        assert_eq!(a.tree().unwrap(), Node::new0(Node::newl(&l)),);
+    }
+
+    #[test]
+    fn leaf1() {
+        let l = Letter {
+            data: vec![0b1000_0000],
+            bit_count: 1,
+        };
+        let a = Alphabet::new(vec![l.clone()]);
+        assert_eq!(a.tree().unwrap(), Node::new1(Node::newl(&l)));
+    }
+
+    #[test]
+    fn multi_byte_letter() {
+        let l = Letter {
+            data: vec![0b1000_0000, 0b1100_0000],
+            bit_count: 10,
+        };
+        let a = Alphabet::new(vec![l.clone()]);
+        assert_eq!(
+            a.tree().unwrap(),
+            Node::new1(Node::new0(Node::new0(Node::new0(Node::new0(Node::new0(
+                Node::new0(Node::new0(Node::new1(Node::new1(Node::newl(&l)))))
+            ))))))
+        );
+    }
+
+    #[test]
+    fn unshared_letters() {
+        let l0 = Letter {
+            data: vec![0b0],
+            bit_count: 1,
+        };
+        let l1 = Letter {
+            data: vec![0b1000_0000],
+            bit_count: 1,
+        };
+        let a = Alphabet::new(vec![l0.clone(), l1.clone()]);
+        assert_eq!(
+            a.tree().unwrap(),
+            Node::new(Node::newl(&l0), Node::newl(&l1))
+        )
+    }
+
+    #[test]
+    fn shared_letters() {
+        let l0 = Letter {
+            data: vec![0b0100_0000],
+            bit_count: 3,
+        };
+        let l1 = Letter {
+            data: vec![0b0110_0000],
+            bit_count: 4,
+        };
+        let a = Alphabet::new(vec![l0.clone(), l1.clone()]);
+        assert_eq!(
+            a.tree().unwrap(),
+            Node::new0(Node::new1(Node::new(
+                Node::newl(&l0),
+                Node::new0(Node::newl(&l1)),
+            )))
+        )
     }
 }
