337285d6fbdaf2903ef7a65f989b91e6ac516864
Prathmesh Prabhu <callpraths@gmail.com>
2020-04-07 23:51:04 -0700

    Add a round trip test for code pack()/parse()
    * Fix two bugs discovered in pack() and add unittest to cover those
    cases.

--- src/code.rs
+++ src/code.rs
@@ -451,7 +451,12 @@ where
                 bytes_written += bw.write(&[byte_buffer]).map_err(|e| e.to_string())?;
 
                 byte_buffer = safe_overflow_leftshift(b, 8 - byte_buffer_len);
-                remaining_bit_count -= 8;
+                if remaining_bit_count > 8 {
+                    remaining_bit_count -= 8;
+                } else {
+                    byte_buffer_len = (byte_buffer_len + remaining_bit_count) % 8;
+                    remaining_bit_count = 0;
+                }
             } else {
                 byte_buffer_len += remaining_bit_count;
                 has_more_bytes = false;
@@ -690,6 +695,28 @@ mod text_pack_tests {
         ];
         let mut got = Vec::new();
         let want: Vec<u8> = vec![0b1101_1000, 0b1110_1110, 0b1110_1000];
+        assert_eq!(pack(letters.into_iter(), &mut got), Ok(want.len()));
+        assert_eq!(got, want);
+    }
+
+    #[test]
+    fn multiple_unaligned_short_letters() {
+        let letters = vec![
+            Letter {
+                data: vec![0b1100_0000],
+                bit_count: 3,
+            },
+            Letter {
+                data: vec![0b1101_0000],
+                bit_count: 6,
+            },
+            Letter {
+                data: vec![0b1100_0000],
+                bit_count: 3,
+            },
+        ];
+        let mut got = Vec::new();
+        let want: Vec<u8> = vec![0b110__1101_0, 0b0__110__0000];
         assert_eq!(pack(letters.into_iter(), &mut got), Ok(want.len()));
         assert_eq!(got, want);
     }
@@ -988,5 +1015,56 @@ mod text_parse_tests {
         let t: Vec<u8> = vec![0x11, 0x11, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01];
         let r: Result<Vec<&Letter>> = parse(&a, Cursor::new(t)).unwrap().collect();
         assert!(r.is_err());
+    }
+}
+
+#[cfg(test)]
+mod text_roundtrip_tests {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn roundtrip() {
+        let l0 = Letter {
+            data: vec![0b1101_1000, 0b1000_0000],
+            bit_count: 9,
+        };
+        let l1 = Letter {
+            data: vec![0b1100_0000],
+            bit_count: 4,
+        };
+        let l2 = Letter::from_bytes(&[0b0101_1101]);
+
+        let alphabet = Alphabet::new(vec![l0.clone(), l1.clone(), l2.clone()]);
+        let text = vec![
+            l1.clone(),
+            l2.clone(),
+            l0.clone(),
+            l2.clone(),
+            l1.clone(),
+            l0.clone(),
+            l1.clone(),
+        ];
+
+        let mut packed = Vec::new();
+        assert!(pack(text.into_iter(), &mut packed).is_ok());
+        let want_packed: Vec<u8> = vec![
+            // l1__l2
+            0b1100__0101,
+            // l2__l0
+            0b1101__1101,
+            // l0__l2
+            0b1000_1__010,
+            // l2__l1
+            0b1_1101__110,
+            // l1__l0
+            0b0__1101_100,
+            // l0_l1_00
+            0b0_1__1100_00,
+        ];
+        assert_eq!(packed, want_packed);
+        let r: Result<Vec<&Letter>> = parse(&alphabet, Cursor::new(packed)).unwrap().collect();
+        let c = r.unwrap();
+        assert_eq!(c, vec![&l1, &l2, &l0, &l2, &l1, &l0, &l1]);
     }
 }
