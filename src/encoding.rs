771f4ea7c4dcbcf13fe02cef9e6f32ca984f21f8
Prathmesh Prabhu <callpraths@gmail.com>
2020-04-25 21:28:06 -0700

    Add methods to extract token set from encoding

--- src/encoding.rs
+++ src/encoding.rs
@@ -14,10 +14,22 @@ pub struct Encoding<T: Token>(HashMap<T, Letter>);
 
 impl<T: Token> Encoding<T> {
     /// The `Alphabet` of `Letter`s used by this `Encoding`.
+    ///
+    /// The `Letter`s are returned in a stable order.
     pub fn alphabet(&self) -> Alphabet {
-        let mut letters: Vec<Letter> = self.0.values().cloned().collect();
-        letters.sort_unstable();
-        Alphabet::new(letters)
+        Alphabet::new(self.sorted_letters().into_iter().cloned().collect())
+    }
+
+    /// The `Token`s covered by this `Encoding`.
+    ///
+    /// The `Token`s are returned in a stable order corresponding to the order
+    /// of `Letter`s in self.alphabet()
+    pub fn tokens(&self) -> Vec<T> {
+        let m = self.reverse_map();
+        self.sorted_letters()
+            .into_iter()
+            .map(|l| m[l].clone())
+            .collect()
     }
 
     /// The `Encoding` map.
@@ -25,5 +37,19 @@ impl<T: Token> Encoding<T> {
     /// Exposes the internal HashMap via an immutable reference.
     pub fn map(&self) -> &HashMap<T, Letter> {
         &self.0
+    }
+
+    fn sorted_letters(&self) -> Vec<&Letter> {
+        let mut letters: Vec<&Letter> = self.0.values().collect();
+        letters.sort();
+        letters
+    }
+
+    fn reverse_map(&self) -> HashMap<&Letter, &T> {
+        let mut m = HashMap::new();
+        for (t, l) in &self.0 {
+            m.insert(l, t);
+        }
+        m
     }
 }
