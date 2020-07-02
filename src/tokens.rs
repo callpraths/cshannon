ee947c9db1ee72de63a407660f5d9f2b158629fd
Prathmesh Prabhu <callpraths@gmail.com>
2020-07-02 22:22:58 +0000

    Make function private

--- src/tokens.rs
+++ src/tokens.rs
@@ -131,9 +131,8 @@ fn pack_u64(s: u64) -> Vec<u8> {
     s.to_be_bytes().to_vec()
 }
 
-// TODO: Make this private.
 // TODO: dedup with code::common::unpack_u64()
-pub fn unpack_u64<R: std::io::Read>(mut r: R) -> Result<u64> {
+fn unpack_u64<R: std::io::Read>(mut r: R) -> Result<u64> {
     let mut buf: [u8; 8] = [0; 8];
     r.read_exact(&mut buf)?;
     Ok(u64::from_be_bytes(buf))
